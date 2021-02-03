c5f936f57a09db0f35ec4d4696f9c0bde1a1631d
Brendan Dutertre <brendan.dutertre@gmail.com>
2021-02-03 19:08:17 +0100

    reglage conflits

--- config/bundles.php
+++ config/bundles.php
@@ -12,10 +12,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-<<<<<<< HEAD
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-=======
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
->>>>>>> develop
 ];
