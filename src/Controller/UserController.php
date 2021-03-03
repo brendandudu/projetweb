3fd2738c4da9716f771228b2e9a819f045ae3840
David Leconte <David.Leconte2@etu.univ-paris1.fr>
2021-03-03 13:34:33 +0100

    Ajout du switch de langues

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
@@ -27,5 +28,4 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
-
 }
