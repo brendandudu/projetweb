f822d4ff95199f6ace53d5bbb2d15c2b9d94b26a
Yanjie SHI <Yanjie.Shi@etu.univ-paris1.fr>
2021-03-12 13:48:50 +0100

    réservation + confirmation mail（à débug)

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,29 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('sex', ChoiceType::class, [
+                'choices' => ['Homme'=>'Homme','Femme'=>'Femme','Autre'=>'Autre'],
+                'required' => true,
+            ])
+            ->add('birthday',DateType::class,[
+                'widget' => 'single_text',
+            ])
+            ->add('phone', TextType::class)
+            ->add('country', ChoiceType::class, [
+                'choices' => ['Allemagne'=>'Allemagne','Afrique du sud'=>'Afrique du sud','Belgique'=>'Belgique','Chine'=>'Chine','États-Unis'=>'États-Unis','France'=>'France','Grèce'=>'Grèce'],
+                'required' => true,
+            ])
+            ->add('city', TextType::class)
+            ->add('avenue', TextType::class)
+            ->add('appartment', TextType::class)
+            ->add('code', TextType::class)
+            ->add('address', TextType::class)
+
+
+
         ;
     }
 
@@ -24,4 +48,20 @@ class UserType extends AbstractType
             'data_class' => User::class,
         ]);
     }
+
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder
+//            ->add('email')
+//            ->add('firstName')
+//            ->add('lastName')
+//        ;
+//    }
+//
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults([
+//            'data_class' => User::class,
+//        ]);
+//    }
 }
