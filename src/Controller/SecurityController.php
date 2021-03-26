28feb0ae4e647606e13c409af28d745c224006ec
David Leconte <David.Leconte2@etu.univ-paris1.fr>
2021-03-26 15:30:25 +0100

    Gestion des mails de confirmation de réservation terminée

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,12 +12,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Security\EmailVerifier;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 /**
  * @Route("/security", name="app_")
  */
 class SecurityController extends AbstractController
 {
+    private $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
     /**
      * @Route("/inscription", name="registration")
      */
@@ -35,7 +48,17 @@ class SecurityController extends AbstractController
             $manager->persist($user);
             $manager->flush();
 
-            return $this->redirectToRoute('app_login');
+            $templatedEmail = new TemplatedEmail();
+            $templatedEmail->from(new Address('betview.conseil@gmail.com', 'Projet RESA (NE PAS RÉPONDRE)'))
+            ->to($user->getEmail())
+            ->subject('Veuillez confirmer votre compte RESA')
+            ->htmlTemplate('security/confirm_email.html.twig');
+
+            $this->emailVerifier->sendEmailConfirmation('confirmation_inscription', $user, $templatedEmail);
+
+            $this->addFlash('success', 'Veuillez vous connecter puis cliquer sur le lien qui vous a été envoyé par mail pour confirmer votre compte.');
+
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('security/register.html.twig', [
@@ -66,5 +89,27 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/verify/email", name="confirmation_inscription")
+     */
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('security_registration');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Votre adresse email a bien été vérifiée.');
+
+        return $this->redirectToRoute('home');
     }
 }
